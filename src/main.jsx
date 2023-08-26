d8554bb2fd0016539dc7323cce5c7a22508d2c1d
Gonzalo Luna <lunagonzalo240mail.com>
2023-08-26 17:41:36 -0500

    1

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './mystyle.css'
-import { HashRouter, BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <HashRouter>
-    <App/>
-  </HashRouter>,
+  <React.StrictMode>
+    <HashRouter>
+      <App />
+    </HashRouter>,
+  </React.StrictMode>
 )
